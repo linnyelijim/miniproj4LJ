9f554bdb16bf1378cc0d4584131334ae6684cc6d
Lindsey Jimenez <lindseyjimenez@gmail.com>
2022-11-02 18:08:44 -0400

    Updated the urls and views to accommodate new html pages.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
-# Create your views here.
+
+# Provides generic list with objects of specified model
+class PostList(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+
+
+# Provides a detailed view for given object of the model
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = 'post_detail.html'
