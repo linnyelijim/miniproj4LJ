5c8e09cbe3b4c29d4faa3d0e508067509ed461fb
Lindsey Jimenez <lindseyjimenez@gmail.com>
2022-11-06 18:11:10 -0500

    Modified contact form and created modal. Created requirements.txt

--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,18 @@
-from django.views import generic
-from .models import Post
 from .forms import CommentForm, ContactForm, NewUserForm
-from django.shortcuts import render, get_object_or_404, redirect
-from django.core.mail import send_mail, BadHeaderError
-from django.http import HttpResponse
-from django.contrib.auth import login, authenticate, logout
+from .models import Post
 from django.contrib import messages
+from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import AuthenticationForm, PasswordResetForm
 from django.contrib.auth.models import User
-from django.template.loader import render_to_string
-from django.db.models.query_utils import Q
-from django.utils.http import urlsafe_base64_encode
 from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import send_mail, BadHeaderError
+from django.db.models.query_utils import Q
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.template.loader import render_to_string
 from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
+from django.views import generic
 
 
 # Provides generic list with objects of specified model
