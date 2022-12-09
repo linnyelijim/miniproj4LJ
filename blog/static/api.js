a0c051d89c1f3bac731bbd1bdd4dc757b399d3ed
Lindsey Jimenez <lindseyjimenez@gmail.com>
2022-12-08 23:27:45 -0500

    Made changes to js and API usage. Added table css.

--- blog/static/api.js
+++ blog/static/api.js
@@ -1,4 +1,4 @@
-const getGames = async (id = 266192) => {
+const getGames = async (id) => {
     try {
         const xmlResponse = await fetch(`https://boardgamegeek.com/xmlapi2/thing?id=${id}`);
         const xml = await xmlResponse.text();
@@ -10,21 +10,13 @@ const getGames = async (id = 266192) => {
             const id = game.getAttribute("id");
             const description = game.querySelector("description").textContent;
             const yearPublished = game.querySelector("yearpublished").getAttribute("value");
-            const minPlayers = game.querySelector("minplayers").getAttribute("value");
+            //const minPlayers = game.querySelector("minplayers").getAttribute("value");
             const maxPlayers = game.querySelector("maxplayers").getAttribute("value");
-            const minPlayTime = game.querySelector("minplaytime").getAttribute("value");
-            const maxPlayTime = game.querySelector("maxplaytime").getAttribute("value");
-            console.log('name - ', name)
-            console.log('id - ', id)
-            console.log('description - ', description)
-            console.log('yearPublished - ', yearPublished)
-            console.log('minPlayers - ', minPlayers)
-            console.log('maxPlayers - ', maxPlayers)
-            console.log('minPlayTime - ', minPlayTime)
-            console.log('maxPlayTime - ', maxPlayTime)
-            // const stats = game.querySelector("stats").getAttribute("stats");
-            // use and return whatever you want
-        })
+            //const minPlayTime = game.querySelector("minplaytime").getAttribute("value");
+            //const maxPlayTime = game.querySelector("maxplaytime").getAttribute("value");
+
+            return {name, id, description, yearPublished, maxPlayers}
+        });
         return gameArray;
     } catch (e) {
         console.log(e);
@@ -47,10 +39,12 @@ const createGames = async (games) => {
 
         let id = createElemWithText('td', game.id);
         let name = createElemWithText('td', game.name);
-        // let stats = createElemWithText('td', game.stats);
+        let description = createElemWithText('td', game.description);
+        let published = createElemWithText('td', game.yearPublished);
+        let players = createElemWithText('td', game.maxPlayers);
 
-        tableRow.append(name, id);
-        fragment.append(tableRow)
+        tableRow.append(name, id, description, published, players);
+        fragment.append(tableRow);
     }
     return fragment;
 }
@@ -61,10 +55,36 @@ const listGames = async (games) => {
     gameInfo.append(element);
     return element;
 }
+/** const getId = async (id) => {
+    const xmlResponse = await fetch(`https://boardgamegeek.com/xmlapi2/thing?id=${id}`);
+    const xml = await xmlResponse.text();
+    const parser = new DOMParser();
+    const xmlDOM = parser.parseFromString(xml, "application/xml");
+    const allGames = [266192, 156129, 1927, 13]
+    let section = xmlDOM.getElementById("gameCards");
+    let classes = xmlDOM.getElementsByClassName("card");
+    let classList = [];
+    for (let i = 0; i < classes.length; i++) {
+        classList.push(classes[i]);
+    }
+
+
+    for (let i = 0; i < allGames.length; i++) {
+        if (classList[i].id === allGames[i]) {
+            let game = await getGames(allGames[i]);
+            await listGames(game);
+        }
+    }
+}
+ */
 
 const hydrateUI = async () => {
-    const games = await getGames()
-    await listGames(games)
+    const allGames = [266192, 156129, 1927, 13];
+    for (let i = 0; i < allGames.length; i++) {
+        let game = await getGames(allGames[i]);
+        await listGames(game);
+    }
 }
 
-hydrateUI()
+hydrateUI();
+
